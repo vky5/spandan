167830481de8caae3934c1d4dbc59cd61a413499
RachitaPant <rachitap89@gmail.com>
2023-11-02 02:01:57 +0530

    Images added for homepage

---
+++ src/img.js
@@ -0,0 +1,79 @@
+import ellipse1 from "..srcImagesEllipse_1.png";
+import ellipse2 from "..srcImagesEllipse_2.png";
+import fb from "..srcImagesfacebook_icon.png";
+import footerBg from "..srcImagesFooter_bg.png";
+import polygonHeader from "..srcImagesheader_polygon";
+import homeBg2 from "..srcImageshomepage_bg2";
+import homeBg from "..srcImageshomepage-bg.png";
+import instaLogo from "..srcImagesInstagram_icon.png";
+import linkedinLogo from "..srcImagesLinkedIn_icon.png";
+import spandanLogo from "..srcImagesSpandan_logo.png";
+import x from "..srcImagesTwitter_icon.png";
+const Projectdata = [
+  {
+    id: 1,
+    name: "Ellipse1",
+    desc: "Timeline background icon",
+    image: "Ellipse_1.png",
+  },
+  {
+    id: 2,
+    name: "El1",
+    desc: "Timeline background icon",
+    image: "Ellipse_2.png",
+  },
+  {
+    id: 3,
+    name: "fb icon for footer",
+    desc: "fb icon",
+    image: "facebook_icon.png",
+  },
+  {
+    id: 4,
+    name: "Footer",
+    desc: "footer background",
+    image: "Footer_bg.png",
+  },
+  {
+    id: 5,
+    name: "polygon_hd",
+    desc: "Polygon icon used under the nav bar",
+    image: "header_polygon.png",
+  },
+  {
+    id: 6,
+    name: "homeBg",
+    desc: "Homepage Background",
+    image: "homepage-bg.png",
+  },
+  {
+    id: 7,
+    name: "Bg2",
+    desc: "Background 2 used on homepage ",
+    image: "homepage_bg2.png",
+  },
+  {
+    id: 8,
+    name: "InstagramLogo",
+    desc: "Instagram Icon for footer",
+    image: "Instagram_icon.png",
+  },
+  {
+    id: 9,
+    name: "LinkedInlogo",
+    desc: "LinkedIn icon for footer",
+    image: "LinkedIn_icon.png",
+  },
+  {
+    id: 10,
+    name: "SpandanLogo",
+    desc: "Spandan logo for header",
+    image: "Spandan_logo.png",
+  },
+  {
+    id: 11,
+    name: "TwiterIcon",
+    desc: "Twitter icon for footer",
+    image: "Twitter_icon.png",
+  },
+];
