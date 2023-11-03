3301ced60ea97a6027fcbf5f84b01823a90bcf96
Nitesh Singh Bisht <niteshsinghbisht03@gmail.com>
2023-11-03 16:43:56 +0530

    added photos and config postcss file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <h1 className="text-red-500">Heellloo</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
