c74791cdd7da1b70db8513f65f06a6fd8ea57ea0
Semicile17 <rohitsinghmahant707@gmail.com>
2023-11-25 20:13:52 +0530

    styling

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -3,29 +3,28 @@ import team from "../Images/team.png";
 import { FacultyData } from "../data/facultyData";
 import { HeadData } from "../data/facultyData";
 import { ManagerData } from "../data/facultyData";
+import '../data/fonts.css'
 const AboutPage = () => {
   return (
     <div className="text-center text-white">
-      <h1 className="py-5 text-xl white text-font-medium md:text-6xl">
+      <h1 className="py-5 text-xl white text-font-medium md:text-6xl"  style={{fontFamily: 'Syne'}}>
         About{" "}
-        <span className="text-2xl text-yellow-500 md:text-7xl">SPANDAN</span>
+        <span className="text-2xl text-yellow-500 md:text-7xl"  style={{fontFamily: 'Syne'}}>SPANDAN</span>
       </h1>
       <div className="text-sm md:text-2xl">
-        <p className="mt-1 font-normal md:font-medium md:mt-5">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris Excepteur sint
-          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-          mollit anim id est laborum
+        <p className="mt-1 font-normal md:font-medium md:mt-5"  style={{fontFamily: 'spacemono'}}>
+        Welcome to SPANDAN-2023, the pinnacle of technological innovation brought to you by GBPIET's Student Activity Cell.
+         As a beacon of cutting-edge advancements, 
+        this festival transcends mere celebration; it's an immersive journey into the pulsating realm of technology.
         </p>
-        <p className="mt-5 font-normal md:font-medium ">
-          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur.
+        <p className="mt-5 font-normal md:font-medium "  style={{fontFamily: 'spacemono'}}>
+        Brace yourself for a riveting showcase featuring epic robo battles, adrenaline-fueled hackathons, 
+        and the exhilarating spectacle of robo soccer. 
+        Dive into the intricate challenges of our line-following robot tracks, designed to push the boundaries of precision and ingenuity.
         </p>
-        <p className="mt-5 font-normal md:font-medium">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates
-          atque omnis recusandae, odit officiis magnam.
+        <p className="mt-5 font-normal md:font-medium"  style={{fontFamily: 'spacemono'}}>
+        2023 beckons you to partake in an unparalleled technological odyssey, where imagination finds flight, competitiveness thrives, and creativity knows no limits. Join us for an unforgettable exploration into the future of technology,
+         marking your calendars for an experience that promises to redefine boundaries and leave an indelible mark on your technological journey.
         </p>
       </div>
       <h2 className="mt-10 text-xl font-semibold md:mt-20 md:text-6xl">
