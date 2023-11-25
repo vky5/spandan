71b483a9b5f46f6f7a2d77fe3fa77d820d525bc3
vky5 <beta@blacktree.comnect.local>
2023-11-25 12:35:30 +0530

    fixed carousel images not loading

--- src/data/slideshow.js
+++ src/data/slideshow.js
@@ -1,61 +1,73 @@
+import img1 from '../Images/homeImages/carousel/1.jpg';
+import img2 from '../Images/homeImages/carousel/2.jpg';
+import img3 from '../Images/homeImages/carousel/3.jpg';
+import img4 from '../Images/homeImages/carousel/4.jpg';
+import img5 from '../Images/homeImages/carousel/5.jpg';
+import img6 from '../Images/homeImages/carousel/6.jpg';
+import img7 from '../Images/homeImages/carousel/7.jpg';
+import img8 from '../Images/homeImages/carousel/8.jpg';
+import img9 from '../Images/homeImages/carousel/9.jpg';
+import img10 from '../Images/homeImages/carousel/10.jpg';
+
+
 const imgSlide = [
     {
       id: 1,
-      src: '../../Images/homeImages/carousel/1.jpg',
+      src: img1,
       alt: "Image 1",
       caption: "Image 1 Description",
     },
     {
       id: 2,
-      src: "src/Images/homeImages/carousel/2.JPG",
+      src: img2,
       alt: "Image 2",
       caption: "Image 2 Description",
     },
     {
       id: 3,
-      src: '/src/Images/homeImages/carousel/3.JPG',
+      src: img3,
       alt: "Image 3",
       caption: "Image 3 Description",
     },
     {
       id: 4,
-      src: '/src/Images/homeImages/carousel/4.JPG',
+      src: img4,
       alt: "Image 4",
       caption: "Image 4 Description",
     },
     {
       id: 5,
-      src: '/src/Images/homeImages/carousel/5.JPG',
+      src: img5,
       alt: "Image 5",
       caption: "Image 5 Description",
     },
     {
       id: 6,
-      src: '/src/Images/homeImages/carousel/6.JPG',
+      src: img6,
       alt: "Image 6",
       caption: "Image 6 Description",
     },
     {
       id: 7,
-      src: '/src/Images/homeImages/carousel/7.JPG',
+      src: img7,
       alt: "Image 7",
       caption: "Image 7 Description",
     },
     {
       id: 8,
-      src: '/src/Images/homeImages/carousel/8.JPG',
+      src: img8,
       alt: "Image 8",
       caption: "Image 8 Description",
     },
     {
       id: 9,
-      src: '/src/Images/homeImages/carousel/9.JPG',
+      src: img9,
       alt: "Image 9",
       caption: "Image 9 Description",
     },
     {
       id: 10,
-      src: '/src/Images/homeImages/carousel/10.JPG',
+      src: img10,
       alt: "Image 10",
       caption: "Image 10 Description",
     },
