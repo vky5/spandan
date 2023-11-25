37fcbd192fbb9d50e2d440ca73269d899504a341
vky5 <beta@blacktree.comnect.local>
2023-11-25 20:38:31 +0530

    box component fixed

--- src/Components/HomeComponents/Box.jsx
+++ src/Components/HomeComponents/Box.jsx
@@ -9,25 +9,26 @@ function Box({ heading, extraHeading, data }) {
     backgroundImage: `url(${bg2})`,
     backgroundSize: "cover",
   };
+
   return (
-    <>
-      <div style={backgroundStyle} className="my-5 mx-5 flex md:h-36">
-        <div className="w-1/3">
-          <div className=" text-yellow-500 font-bold pt-9 pl-5 text-l" style={{fontFamily: 'Syne'}}>
-            {heading}
-          </div>
-          <div className="text-white font-bold text-sm pl-5" style={{fontFamily: 'Syne'}}>
-            {extraHeading}
-          </div>
+    <div style={backgroundStyle} className="my-5 mx-5 flex md:h-36 items-center">
+      <div className="w-1/3 text-center">
+        <div className="text-yellow-500 font-bold text-lg" style={{ fontFamily: 'Syne' }}>
+          {heading}
         </div>
-        <div className="w-1/10 h-16 pl-3 pt-6">
-          <img src={line} className="h-24" />
-        </div>
-        <div className="w-10/12 p-4 text-white text-xs whitespace-normal" style={{fontFamily: 'spacemono'}}>
-          {data}
+        <div className="text-white font-bold text-sm" style={{ fontFamily: 'Syne' }}>
+          {extraHeading}
         </div>
       </div>
-    </>
+      <div className="w-1/10 flex items-center justify-center">
+        <div className="h-full border-l border-white">
+          <img src={line} className="h-24" alt="line" />
+        </div>
+      </div>
+      <div className="w-10/12 p-4 text-white text-xs whitespace-normal" style={{ fontFamily: 'spacemono' }}>
+        {data}
+      </div>
+    </div>
   );
 }
 
